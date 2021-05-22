77872f2f981e9910df47c9ec862711ed83a31b1a
PoHandsome <louisfat54108@gmail.com>
2021-05-22 13:31:10 +0930

    filter

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 data = []
 y = 0
+count = 0
 
 with open ('reviews.txt', 'r') as f:
     for l in f:
@@ -10,3 +11,17 @@ avg = y / len(data)
 
 print('留言平均長度為', avg)
 
+
+for d in data:
+    if len(d) >= 100:
+        count += 1
+    else:
+        pass
+
+print('總共有', count, '筆資料長度為100以上')
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('總共有', len(good), '筆資料提到good')
